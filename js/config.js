fe26a16f04bcf1ff0c0ce373114a94be25515a33
vava <58034771+vavacc@users.noreply.github.com>
2025-07-08 15:32:19 +0800

    config.js

--- js/config.js
+++ js/config.js
@@ -111,7 +111,8 @@ const API_SITES = {
     },
     guangsu: {
         api: 'https://api.guangsuapi.com/api.php/provide/vod',
-        name: '光速资源'
+        name: '光速资源',
+        detail: 'https://www.guangsuzy.com'
     },
     taopian: {
         api: 'https://taopianapi.com/cjapi/sda/vod',
@@ -151,7 +152,8 @@ const API_SITES = {
     },
     jyzy: {
         api: 'https://jyzyapi.com/provide/vod',
-        name: '金鹰资源'
+        name: '金鹰资源',
+        detail: 'https://jyzyapi.comm'
     },
     kuaiche: {
         api: 'https://caiji.kuaichezy.org/api.php/provide/vod',
@@ -167,7 +169,8 @@ const API_SITES = {
     },
     huya: {
         api: 'https://www.huyaapi.com/api.php/provide/vod',
-        name: '虎牙资源'
+        name: '虎牙资源',
+        detail: 'https://huyazy.net'
     },
     maota: {
         api: 'https://caiji.maotaizy.cc/api.php/provide/vod',
@@ -175,7 +178,8 @@ const API_SITES = {
     },
     xinlang: {
         api: 'https://api.xinlangapi.com/xinlangapi.php/provide/vod',
-        name: '新浪资源'
+        name: '新浪资源',
+        detail: 'https://www.xinlangzy.com'
     },
     fantuan: {
         api: 'https://www.fantuan.tv/api.php/provide/vod',
@@ -195,28 +199,23 @@ const API_SITES = {
     },
      jkun: {
          api: 'https://jkunzyapi.com',
-         name: 'jkun资源',
-         adult: true
+         name: 'jkun资源'
      },
      bwzy: {
          api: 'https://api.bwzym3u8.com',
-         name: '百万资源',
-         adult: true
+         name: '百万资源'
      },
      souav: {
          api: 'https://api.souavzy.vip',
-         name: 'souav资源',
-         adult: true
+         name: 'souav资源'
      },
      r155: {
          api: 'https://155api.com',
-         name: '155资源',
-         adult: true
+         name: '155资源'
      },
      lsb: {
          api: 'https://apilsbzy1.com',
-         name: 'lsb资源',
-         adult: true
+         name: 'lsb资源'
      },
      huangcang: {
          api: 'https://hsckzy.vip',
